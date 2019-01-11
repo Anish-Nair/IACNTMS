f1b1773bc38b98b24796f1adc35c2da314bf13af
anishpnair1985 <39954189+anishpnair1985@users.noreply.github.com>
2019-01-11 17:16:03 +0530

    Add files via upload

---
+++ function.ps1
@@ -0,0 +1,18 @@
+﻿function get-inventory{
+param (
+$computername = 'localhost',
+$drivetype = 3
+)
+$savefile =$env:USERPROFILE + ‘\Desktop\’ +((Get-Date).ToString(“dd_MM_yyyy_HHmm”))+’.csv’
+
+
+$disk1 = Get-WmiObject -class Win32_LogicalDisk -computername $computername -filter "drivetype=$drivetype" |Select-Object -property DeviceID,
+@{name='FreeSpace(GB)';expression={$_.FreeSpace / 1MB -as [int]}},
+@{name='Size(GB)';expression={$_.Size / 1GB -as [int]}},
+@{name='%Free';expression={$_.FreeSpace / $_.Size * 100 -as [int]}}
+
+
+$Report = $disk1 |Export-csv $savefile -NoTypeInformation
+
+
+}
