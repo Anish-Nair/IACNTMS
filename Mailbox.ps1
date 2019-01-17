944dbd193936a39198097a3716e619cf591d5aa1
anishpnair1985 <39954189+anishpnair1985@users.noreply.github.com>
2019-01-17 10:33:42 +0530

    Add files via upload

---
+++ Mailbox.ps1
@@ -0,0 +1,32 @@
+﻿
+function get-mailboxlist
+{
+ 
+param(
+[parameter(mandatory = $true)]
+[string]$identity
+)
+#$Mailboxes = Import-Csv -Path D:\scripts\mal.csv
+foreach($mailbox in $identity)
+{$Used = Get-MailboxStatistics -Identity $mailbox
+$TotalQuota = Get-Mailbox -Identity $mailbox
+
+$MBX = [pscustomobject] @{
+Name = ($used).Displayname;
+Created = [string] ($TotalQuota).whencreated;
+Valid = ($used).Isvalid;
+OU= ($TotalQuota).organizatonalunit
+'Total Size' = ($TotalQuota).ProhibitSendReceiveQuota;
+'Used Space' = ($Used).TotalItemSize;
+Database = ($used).databasename
+Hidden = ($TotalQuota).Hiddenfromaddresslistsenabled
+'Forward To' = ($TotalQuota).forwardingaddress
+#'%Free'= (($used).TotalItemSize / ($TotalQuota).ProhibitSendReceiveQuota) * 100
+#'%free' = ('Used space' / 'Total Size') * 100 -as [int]
+#'%Free' = @{name='%Free';expression={$_.FreeSpace / $_.Size * 100 -as [int]}}
+}
+
+$MBX | Export-Csv -Path D:\scripts\mailtest.csv -NoTypeInformation
+}
+
+}
